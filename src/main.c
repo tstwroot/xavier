550be31dfabc97226fc79667d9ddd470b11633c7
tstwroot <tstwroot@protonmail.com>
2024-12-26 23:01:57 -0400

    Add: searches range of addresses and shows their values

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "memory.h"
 #include "banner.h"
 
 int main(int argc, char **argv)
 {
-  printf("%s\n", XAVIER_BANNER);
+  if(argc != 2)
+    return -1;
+
+  puts(XAVIER_BANNER);
+  pid_t pid = atoi(argv[1]);
+  mscan(pid);
   return 0;
 }
